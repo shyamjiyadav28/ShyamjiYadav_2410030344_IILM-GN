9c95fd23d39f2f4e83568623d236319c135954cb
Shyamji Yadav <shyamji.yadav.cs28@iilm.edu>
2026-02-20 22:42:12 +0530

    Add files via upload

---
+++ Group_Anagrams.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            key = ''.join(sorted(word))
+            anagram_map[key].append(word)
+        
+        return list(anagram_map.values()) 
