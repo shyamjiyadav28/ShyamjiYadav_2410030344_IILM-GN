51198a78724a2faf8c08dd82638d2e65f0a93263
Shyamji Yadav <shyamji.yadav.cs28@iilm.edu>
2026-04-26 22:19:23 +0530

    Create Nth Fibonacci Number.py

---
+++ Nth Fibonacci Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def nthFibonacci(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        a, b = 0, 1
+        for i in range(2, n + 1):
+            a, b = b, a + b
+        
+        return b
